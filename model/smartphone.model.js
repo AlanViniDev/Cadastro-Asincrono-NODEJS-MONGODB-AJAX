64b606e92be40b7cb17ee45040646bdb8f006d6e
Alan Vinicius <alan123vinicius@gmail.com>
2020-06-05 14:47:43 -0300

    Add files via upload

---
+++ model/smartphone.model.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+let SmartphoneSchema = new Schema({
+    nome: {type: String, required: true, max: 100},
+    marca: {type: String, required: true, max: 100},  
+});
+// Exportar o modelo
+module.exports = mongoose.model('Smartphone', SmartphoneSchema);
